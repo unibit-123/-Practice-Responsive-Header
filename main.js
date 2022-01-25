ef518b5217664067e444a162350f5c487a724262
unibit-123 <piuungshuung@gmail.com>
2022-01-25 16:16:35 +0900

    tootle issue

---
+++ main.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector('.navbar_toogleBtn');
+const menu = document.querySelector('.navbar_menu');
+const icons = document.querySelector('.navbar_icons');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.tootle('active');
+    icons.classList.tootle('active');
+});
